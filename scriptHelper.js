f3aea1e3558d273d2daef0810087007820ccd3a8
Darren LaCour <lacourd@icloud.com>
2023-03-06 22:20:51 -0600

    big breakthrough

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,39 +2,65 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+   missionTarget = document.getElementById("missionTarget")
+    missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">
+                `
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === "") {return "Empty"}
+    else if (isNaN(parseInt(testInput)) === true) {return "Not a Number"}
+    else if (typeof(parseInt(testInput)) === "number") {return "Is a Number"} 
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    console.log("Something correct has happened!");
+    
+    let pilotValidation = validateInput(pilot);
+    let copilotValidation = validateInput(copilot);
+    let fuelLevelValidation = validateInput(fuelLevel);
+    let cargoLevelValidation = validateInput(cargoLevel);
+
+    if (pilotValidation === "Empty" || copilotValidation === "Empty" || fuelLevelValidation === "Empty" || cargoLevelValidation === "Empty") {
+        alert("All fields are required!");
+        return
+    }
+    if (fuelLevelValidation !== "Is a Number") {
+        alert("Please enter the amount of fuel in Liters");
+        return
+    }
+    if (cargoLevelValidation !== "Is a Number") {
+        alert("Please enter the cargo mass in kilograms");
+        return
+    }
+    
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json()});
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let picker = Math.floor(Math.random()*planets.length);
+    return planets[picker];
 }
+
+
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
